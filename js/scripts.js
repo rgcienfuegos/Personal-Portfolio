9ae2bc3a8403b78040ae8955a8c9af1f4adb4e1a
rgcienfuegos <rgcienfuegos@hotmail.com>
2020-08-01 15:22:01 +0200

    v0.7

--- js/scripts.js
+++ js/scripts.js
@@ -17,32 +17,36 @@ async function getRepos() {
     let counter = 0;
 
     result.forEach(i => {
+if(!i.name.includes("rgcienfuegos") ){
+    
+    counter = counter + 1;
 
-        counter = counter + 1;
+    let repoHTML = `
+     <h2 id='nameRepo` + counter + `'></h2>
+     <p id='descRepo` + counter + `'></p>
+     <p>
+       <a id='butRepoDet` + counter + `' class="btn btn-secondary" href="#" role="button">Mas info&raquo;</a>
+       <a id='butRepoDem` + counter + `' class="btn btn-secondary" href="https://rgcienfuegos.github.io/` + i.name + `//" role="button">Link demo &raquo;</a>
+     </p>
+     `
+    let div = document.createElement("div");
 
-        let repoHTML = `
-         <h2 id='nameRepo` + counter + `'></h2>
-         <p id='desRepo` + counter + `'></p>
-         <p>
-           <a id='butRepoDet` + counter + `' class="btn btn-secondary" href="#" role="button">Mas info&raquo;</a>
-           <a id='butRepoDem` + counter + `' class="btn btn-secondary" href="https://rgcienfuegos.github.io/` + i.name + `//" role="button">Link demo &raquo;</a>
-         </p>
-         `
-        let div = document.createElement("div");
+    div.innerHTML = repoHTML;
+    div.className = "col-md-4";
 
-        div.innerHTML = repoHTML;
-        div.className = "col-md-4";
-
-        divResult.appendChild(div)
-        let nameRepo = document.getElementById("nameRepo" + counter);
-        nameRepo.href = i.html_url;
-        nameRepo.textContent = i.name;
-        let desRepo = document.getElementById("desRepo" + counter);
-        desRepo.textContent = i.description;
+    divResult.appendChild(div)
+    let nameRepo = document.getElementById("nameRepo" + counter);
+    nameRepo.href = i.html_url;
+    nameRepo.textContent = i.name;
+    let desRepo = document.getElementById("descRepo" + counter);
+    desRepo.textContent = i.description;
 
 
+}
+        
 
     })
+}
 
 // ---------Responsive-navbar-active-animation-----------
 function test(){
@@ -84,4 +88,3 @@ function test(){
     setTimeout(function(){ test(); });
   });
 
-}
