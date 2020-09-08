6a8b5c3f4ac90c10d970b44e4e6b8bfd52cb75b1
rgcienfuegos <rgcienfuegos@hotmail.com>
2020-09-08 18:17:07 +0200

    prettier

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
 const projectName = 'portfolio';
 
 function clear() {
-    while (divResult.firstChild)
-        divResult.removeChild(divResult.firstChild)
+  while (divResult.firstChild)
+    divResult.removeChild(divResult.firstChild)
 }
 async function getRepos() {
-    clear();
-    const url = "https://api.github.com/users/rgcienfuegos/repos"
-    const response = await fetch(url)
-    const result = await response.json()
-    let counter = 0;
+  clear();
+  const url = "https://api.github.com/users/rgcienfuegos/repos"
+  const response = await fetch(url)
+  const result = await response.json()
+  let counter = 0;
 
-    result.forEach(i => {
-if(!i.name.includes("rgcienfuegos")&&!i.fork&&!i.private){    
-    counter = counter + 1;
-    let repoHTML = `
+  result.forEach(i => {
+    if (!i.name.includes("rgcienfuegos") && !i.fork && !i.private) {
+      counter = counter + 1;
+      let repoHTML = `
      <h2 id='nameRepo` + counter + `'></h2>
      <p id='descRepo` + counter + `'></p>
      <img class="project-image"
@@ -23,100 +23,105 @@ if(!i.name.includes("rgcienfuegos")&&!i.fork&&!i.private){
      <p class="project-title">
        <a id='butRepoDem` + counter + `' class="btn " href="https://rgcienfuegos.github.io/` + i.name + `//" role="button">Link &raquo;</a>
      </p>
-     `     
-     let div = document.createElement("div");
+     `
+      let div = document.createElement("div");
 
-    div.innerHTML = repoHTML;
-    div.className = "project";
+      div.innerHTML = repoHTML;
+      div.className = "project";
 
-    divResult.appendChild(div)
-    let nameRepo = document.getElementById("nameRepo" + counter);
-    nameRepo.href = i.html_url;
-    nameRepo.textContent = i.name;
-    let desRepo = document.getElementById("descRepo" + counter);
-    desRepo.textContent = i.description;
-}
-        
+      divResult.appendChild(div)
+      let nameRepo = document.getElementById("nameRepo" + counter);
+      nameRepo.href = i.html_url;
+      nameRepo.textContent = i.name;
+      let desRepo = document.getElementById("descRepo" + counter);
+      desRepo.textContent = i.description;
+    }
 
-    })
+
+  })
 }
 
 // ---------Responsive-navbar-active-animation-----------
-function test(){
-    let tabsNewAnim = $('#navbarSupportedContent');
-    let selectorNewAnim = $('#navbarSupportedContent').find('li').length;
-    let activeItemNewAnim = tabsNewAnim.find('.active');
-    let activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
-    let activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
-    let itemPosNewAnimTop = activeItemNewAnim.position();
-    let itemPosNewAnimLeft = activeItemNewAnim.position();
-    let itemText= $(this).first().text();
-      let itemColor;
-      if (itemText.includes("Proyectos")|| itemText.includes("Referencias") ) {
-         itemColor= "var(--main-gray2)";
-      } else {
-        itemColor= "var(--main-gray)";
-      }
+function test() {
+  let tabsNewAnim = $('#navbarSupportedContent');
+  let selectorNewAnim = $('#navbarSupportedContent').find('li').length;
+  let activeItemNewAnim = tabsNewAnim.find('.active');
+  let activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
+  let activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
+  let itemPosNewAnimTop = activeItemNewAnim.position();
+  let itemPosNewAnimLeft = activeItemNewAnim.position();
+  let itemText = $(this).first().text();
+  let itemColor;
+  if (itemText.includes("Proyectos") || itemText.includes("Referencias")) {
+    itemColor = "var(--main-gray2)";
+  } else {
+    itemColor = "var(--main-gray)";
+  }
 
+
+  $(".hori-selector").css({
+    "top": itemPosNewAnimTop.top + "px",
+    "left": itemPosNewAnimLeft.left + "px",
+    "height": activeWidthNewAnimHeight + "px",
+    "width": activeWidthNewAnimWidth + "px",
+    "background-color": itemColor
+  });
+
+
+  $(".hori-selector .right").css({
+    "background-color": itemColor
+  });
+  $(".hori-selector .left").css({
+    "background-color": itemColor
+  });
+
+  $("#navbarSupportedContent").on("click", "li", function (e) {
+    $('#navbarSupportedContent ul li').removeClass("active");
+    $(this).addClass('active');
+    let activeWidthNewAnimHeight = $(this).innerHeight();
+    let activeWidthNewAnimWidth = $(this).innerWidth();
+    let itemPosNewAnimTop = $(this).position();
+    let itemPosNewAnimLeft = $(this).position();
+    let itemText = $(this).first().text();
+    let itemColor;
+    if (itemText.includes("Proyectos") || itemText.includes("Referencias")) {
+      itemColor = "var(--main-gray2)";
+    } else {
+      itemColor = "var(--main-gray)";
+    }
 
     $(".hori-selector").css({
-      "top":itemPosNewAnimTop.top + "px", 
-      "left":itemPosNewAnimLeft.left + "px",
+      "top": itemPosNewAnimTop.top + "px",
+      "left": itemPosNewAnimLeft.left + "px",
       "height": activeWidthNewAnimHeight + "px",
       "width": activeWidthNewAnimWidth + "px",
+      "background-color": itemColor
+
+    });
+
+    $(".hori-selector .right").css({
+      "background-color": itemColor
+    });
+    $(".hori-selector .left").css({
       "background-color": itemColor
     });
 
 
-    $(".hori-selector .right").css({
-      "background-color": itemColor      
-    });
-    $(".hori-selector .left").css({
-      "background-color": itemColor      
-    });
-
-    $("#navbarSupportedContent").on("click","li",function(e){
-      $('#navbarSupportedContent ul li').removeClass("active");
-      $(this).addClass('active');
-      let activeWidthNewAnimHeight = $(this).innerHeight();
-      let activeWidthNewAnimWidth = $(this).innerWidth();
-      let itemPosNewAnimTop = $(this).position();
-      let itemPosNewAnimLeft = $(this).position();
-      let itemText= $(this).first().text();
-      let itemColor;
-      if (itemText.includes("Proyectos")|| itemText.includes("Referencias") ) {
-         itemColor= "var(--main-gray2)";
-      } else {
-        itemColor= "var(--main-gray)";
-      }
-     
-      $(".hori-selector").css({
-        "top":itemPosNewAnimTop.top + "px", 
-        "left":itemPosNewAnimLeft.left + "px",
-        "height": activeWidthNewAnimHeight + "px",
-        "width": activeWidthNewAnimWidth + "px",
-        "background-color": itemColor
-      
-      });
-
-      $(".hori-selector .right").css({
-        "background-color": itemColor      
-      });
-      $(".hori-selector .left").css({
-        "background-color": itemColor      
-      });
-
-
-    });
-  }
-  $(document).ready(function(){
-    setTimeout(function(){ test(); });
-    getRepos() ;
   });
-  $(window).on('resize', function(){
-    setTimeout(function(){ test(); }, 500);
+}
+$(document).ready(function () {
+  setTimeout(function () {
+    test();
   });
-  $(".navbar-toggler").click(function(){
-    setTimeout(function(){ test(); });
+  getRepos();
+});
+$(window).on('resize', function () {
+  setTimeout(function () {
+    test();
+  }, 500);
+});
+$(".navbar-toggler").click(function () {
+  setTimeout(function () {
+    test();
   });
-
+});
